2e2229183dfafbb3cfe7378b04dd1d1e14b1cea3
tobbelobb <tobben@fastmail.fm>
2019-05-30 18:47:22 +0200

    Renames build.sh to compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
-mkdir build
+#!/usr/bin/env bash
+
+mkdir -p build
 cd build
 CC=gcc-9 CXX=g++-9 cmake ..
-cmake --build
+cmake --build .
 
 #g++-9 -std=c++2a src/line_collision_detector.cpp -Iinclude -o line_collision_detector
