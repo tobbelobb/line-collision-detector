2b0ec92394f447ceefe935a3142769df3b6d1cba
tobbelobb <tobben@fastmail.fm>
2019-06-03 18:26:31 +0200

    Clean only selected directories with clean.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-rm -rf build
+rm -rf build/line_collision_detector
+echo "Removing build/line_collision_detector"
+rm -rf  build/CMakeFiles/line_collision_detector.dir
+echo "Removing build/CMakeFiles/line_collision_detector.dir"
+rm -rf build/test
+echo "Removing build/test"
