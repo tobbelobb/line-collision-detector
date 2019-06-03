474dd639363ac43adbc4ee81fa3282e79543c961
tobbelobb <tobben@fastmail.fm>
2019-06-03 18:27:00 +0200

    Compiles only specified targets with compile.sh

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@
 mkdir -p build
 cd build
 CC=gcc CXX=g++ cmake ..
-cmake --build .
+make -j 4 line_collision_detector
 
 #g++-9 -std=c++2a src/line_collision_detector.cpp -Iinclude -o line_collision_detector
