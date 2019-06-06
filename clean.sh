4bbe7988a9de9738da8c36127a436a94d038d44d
tobbelobb <tobben@fastmail.fm>
2019-06-06 15:16:16 +0200

    Indents bash code with shfmt

--- clean.sh
+++ clean.sh
@@ -2,7 +2,7 @@
 
 rm -rf build/line_collision_detector
 echo "Removing build/line_collision_detector"
-rm -rf  build/CMakeFiles/line_collision_detector.dir
+rm -rf build/CMakeFiles/line_collision_detector.dir
 echo "Removing build/CMakeFiles/line_collision_detector.dir"
 rm -rf build/test
 echo "Removing build/test"
--- indent.sh
+++ indent.sh
@@ -8,3 +8,8 @@ find . -name CMakeLists.txt -exec cmake-format --config-file .cmake-format.json 
 
 # Requires clang-format
 find . -regex ".*\.\(h\|cpp\)" -not -path "./build/*" -not -path "./extern/*" -exec clang-format -i {} +
+
+# Requires shfmt
+# Install with:
+#   sudo snap install shfmt
+shfmt -w ./*.sh
