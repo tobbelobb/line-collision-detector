cdbd69e03d89b8e67ef234ca40a39e0fb58e50e9
Torbjørn Ludvigsen <tobben@fastmail.fm>
2020-05-21 19:45:35 +0200

    Makes tidy.sh accept one argument

--- tidy.sh
+++ tidy.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+FILES="/line-collision-detector/linc/linc"
+if [ $1 ]; then
+	# Cut away trailing ++ because run-clang-tidy will interpret them as a regex
+	FILES=${1%++}
+fi
+
 # This script is executable from within the docker image
 ./make_compilation_database.sh
 cd /line-collision-detector/linc
-run-clang-tidy-10 -p=.. -checks="-*,cppcoreguidelines-*,modernize-*,bugprone-*,clang-analyzer-*,misc-*,performance-*,readability-*,cert-*,hicpp-*" -fix "/line-collision-detector/linc/linc/*" 2>/dev/null
+run-clang-tidy-10 -p=.. -checks="-*,cppcoreguidelines-*,modernize-*,bugprone-*,clang-analyzer-*,misc-*,performance-*,readability-*,cert-*" -fix $FILES 2>/dev/null
