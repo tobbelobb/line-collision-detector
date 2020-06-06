b8d5331d3fe34c6a75cb9ab601a14efb0dd02fe7
Torbjørn Ludvigsen <tobben@fastmail.fm>
2020-06-06 16:33:44 +0200

    Run indent from anywhere

--- indent.sh
+++ indent.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # This script is executable from within the docker image
+cd /line-collision-detector/
 
 find . -regex ".*\.\(h\+\+\|c\+\+\)" -not -path "./build/*" -not -path "./linc/extern/*" -not -path "./linc-gcc/*" -not -path "./linc-clang/*" -exec clang-format-10 -i {} +
 
