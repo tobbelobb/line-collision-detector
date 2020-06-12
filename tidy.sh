bed1d8b842e9b04f7829bb1d143c9cbe76e54c2c
Torbjørn Ludvigsen <tobben@fastmail.fm>
2020-06-12 13:55:12 +0200

    Makes tidy not check magic numbers in tests

--- tidy.sh
+++ tidy.sh
@@ -1,12 +1,33 @@
 #!/usr/bin/env bash
 
-FILES="/line-collision-detector/linc/linc"
+# Find only test files (absolute paths)
+TESTS="/line-collision-detector/linc/linc/.*test\.c.*"
+
+# Find all .c++ files except test files (absolute paths)
+APPLICATION_CODE="/line-collision-detector/linc/linc/([a-z]|-|_|[A-Z]|[0-9])+\.c"
+
+# If there's an argument, use it and assume it is application code
 if [ $1 ]; then
+  # Convert a relative path to an absolute path
+  APPLICATION_CODE=`find /line-collision-detector/linc/ -name "*$1*"`
 	# Cut away trailing ++ because run-clang-tidy will interpret them as a regex
-	FILES=${1%++}
+	APPLICATION_CODE=${APPLICATION_CODE%++}
+  echo $APPLICATION_CODE
 fi
+
+CHECKS="-*,cppcoreguidelines-*,modernize-*,bugprone-*,clang-analyzer-*,misc-*,performance-*,readability-*"
 
 # This script is executable from within the docker image
 /line-collision-detector/make-compilation-database.sh
 cd /line-collision-detector/linc
-run-clang-tidy-10 -p=.. -checks="-*,cppcoreguidelines-*,modernize-*,bugprone-*,clang-analyzer-*,misc-*,performance-*,readability-*" -fix $FILES 2>/dev/null
+
+echo "Check application code"
+run-clang-tidy-10 -p=.. -checks=$CHECKS -fix -quiet $APPLICATION_CODE 2>/dev/null
+
+if [ -z "$1" ]; then
+  # Allow magic numbers in test code
+  CHECKS="$CHECKS,-readability-magic-numbers,-cppcoreguidelines-avoid-magic-numbers"
+  echo "
+  Check test code"
+  run-clang-tidy-10 -p=.. -checks=$CHECKS -fix -quiet $TESTS 2>/dev/null
+fi
