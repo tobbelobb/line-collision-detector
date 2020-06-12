7dc32526b2aed103d449cb3a0f9d3ba3e9dd6dda
Torbjørn Ludvigsen <tobben@fastmail.fm>
2020-06-12 21:10:54 +0200

    Adds vimmy buildscript

---
+++ bv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# |& pipes both stdout and stderr
+
+b "$@" |&
+  awk '{
+         line=$0;
+         ORS="";
+         if (line ~/(\.c|\.cpp|\.h|\.c\+\+|\.h\+\+):[0-9]+:[0-9]+:.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?(note|error):/)
+         {
+           n=split(line, a, ":");
+           print("vim " a[1] " +" a[2] " ;# ");
+           print(a[3]);
+           for(i=4;i<n+1;i++){
+             print(":" a[i])
+           }
+           print("\n");
+         }
+         else
+         {
+             print(line "\n");
+         };
+       }' | head -n 40;
